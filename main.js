ad8437fd26fd7a3400316ed9e9f7d397a0b7c159
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-08-10 16:04:19 -0400

    Week-3-Project: Most bugs out and it functions basically. Need to account for any operators at the end of an equation and limit the size of the results(will round to ten or hundredths of a thousand.

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-let keys = document.getElementsByClassName('button');
-let screen = document.querySelector('span');
+let keys = document.querySelectorAll('.key');
+let screen = document.querySelector('.screen');
+
+for (let i in keys){
+  keys[i].onclick = function () {
+    let keyInput = screen.innerHTML;
+    let equation = keyInput;
+    let keyValue = this.innerHTML;
+    let oper = document.querySelectorAll('.operator');
+    let operators = oper.innerHTML;
+
+    event.preventDefault();     //CHECK ON THIS
+
+    screen.innerHTML += keyValue;
+
+    if (keyValue === 'C') {
+      screen.innerHTML = '';
+    }
+    else if (keyValue === '=') {
+      equation = keyInput;
+      screen.innerHTML = eval(equation);
+
+    }
+
+
+
+    console.log(keys[i]);
+    console.log(keyInput);
+    console.log(equation);
+
+  } //onclick function end
+} //for loop end
+//======fin==============
